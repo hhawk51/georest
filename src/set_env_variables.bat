0579e822933b02e087950f652105e9647e5cc48d
jumpinjackie <jumpinjackie@localhost>
2012-02-09 20:45:36 +1100

    Issue 26: Externalise the fdo include/linker paths so that we can use the official FDO SDKs.

--- src/set_env_variables.bat
+++ src/set_env_variables.bat
@@ -5,15 +5,66 @@ SET LIBKML_PATH=%CD%\Thirdparty\libkml-1.0.1
 SET ZLIB_PATH=%CD%\Thirdparty\zlib-1.2.3
 SET FASTCGI_PATH=%CD%\Thirdparty\fcgi-2.4.0
 
-SET FDO_PATH_3_4=D:/Development/FDO_SVN/branches/3.4
-SET FDO_PATH_3_5=D:/Development/FDO_SVN/branches/3.5
-REM SET FDO_PATH=$(FDO_PATH_3_5)
-SET FDO_PATH=D:/fdo-trunk
+REM Non-SDK Include Paths (x86, x64)
+REM SET FDO_INC_PATH_3_4=D:/fdo-3.4/Fdo/Unmanaged/Inc
+REM SET FDO_EXPRENGINE_INC_PATH_3_4=D:/fdo-3.4/Utilities/ExpressionEngine/Inc
+REM SET FDO_INC_PATH_3_5=D:/fdo-3.5/Fdo/Unmanaged/Inc
+REM SET FDO_EXPRENGINE_INC_PATH_3_5=D:/fdo-3.5/Utilities/ExpressionEngine/Inc
+REM SET FDO_INC_PATH_3_6=D:/fdo-3.6/Fdo/Unmanaged/Inc
+REM SET FDO_EXPRENGINE_INC_PATH_3_6=D:/fdo-3.6/Utilities/ExpressionEngine/Inc
+REM SET FDO_INC_PATH=%FDO_INC_PATH_3_6%
+REM SET FDO_EXPRENGINE_INC_PATH=%FDO_EXPRENGINE_INC_PATH_3_6%
 
-SET MG_PATH_OS_2_1=D:/Development/MapGuideSVN/branches/2.1/MgDev
-SET MG_PATH_OS_2_2=D:/Development/MapGuideSVN/branches/2.2/MgDev
-SET MG_PATH_ENT_2010=D:/Development/MapGuideSVN/sandbox/adsk/2.1
-SET MG_PATH_ENT_2011=D:/Development/MapGuideSVN/sandbox/adsk/2.2gp
+REM Non-SDK lib paths (Release)
+REM SET FDO_LIB_PATH_3_4=D:/fdo-3.4/Fdo/Unmanaged/Lib/Win32/Release
+REM SET FDO_LIB_PATH_3_5=D:/fdo-3.5/Fdo/Unmanaged/Lib/Win32/Release
+REM SET FDO_LIB_PATH_3_6=D:/fdo-3.6/Fdo/Unmanaged/Lib/Win32/Release
+REM SET FDO_LIB_PATH=%FDO_LIB_PATH_3_6%
+REM SET FDO_LIB_PATH64_3_5=D:/fdo-3.5/Fdo/Unmanaged/Lib/Win64/Release
+REM SET FDO_LIB_PATH64_3_6=D:/fdo-3.6/Fdo/Unmanaged/Lib/Win64/Release
+REM SET FDO_LIB_PATH64=%FDO_LIB_PATH64_3_6%
+
+REM Non-SDK lib paths (Debug)
+REM SET FDO_LIB_PATH_DEBUG_3_4=D:/fdo-3.4/Fdo/Unmanaged/Lib/Win32/Debug
+REM SET FDO_LIB_PATH_DEBUG_3_5=D:/fdo-3.5/Fdo/Unmanaged/Lib/Win32/Debug
+REM SET FDO_LIB_PATH_DEBUG_3_6=D:/fdo-3.6/Fdo/Unmanaged/Lib/Win32/Debug
+REM SET FDO_LIB_PATH_DEBUG=%FDO_LIB_PATH_DEBUG_3_6%
+REM SET FDO_LIB_PATH64_DEBUG_3_5=D:/fdo-3.5/Fdo/Unmanaged/Lib/Win64/Debug
+REM SET FDO_LIB_PATH64_DEBUG_3_6=D:/fdo-3.6/Fdo/Unmanaged/Lib/Win64/Debug
+REM SET FDO_LIB_PATH64_DEBUG=%FDO_LIB_PATH64_DEBUG_3_6%
+
+REM SDK Include Paths (x86, x64)
+SET FDO_INC_PATH_3_4=D:/fdo-sdk/x86/3.4/Inc
+SET FDO_EXPRENGINE_INC_PATH_3_4=%FDO_INC_PATH_3_4%/ExpressionEngine
+SET FDO_INC_PATH_3_5=D:/fdo-sdk/x86/3.5/Inc
+SET FDO_EXPRENGINE_INC_PATH_3_5=%FDO_INC_PATH_3_5%/ExpressionEngine
+SET FDO_INC_PATH_3_6=D:/fdo-sdk/x86/3.6/Inc
+SET FDO_EXPRENGINE_INC_PATH_3_6=%FDO_INC_PATH_3_6%/ExpressionEngine
+SET FDO_INC_PATH=%FDO_INC_PATH_3_6%
+SET FDO_EXPRENGINE_INC_PATH=%FDO_INC_PATH%/ExpressionEngine
+
+REM SDK Lib Paths (Release)
+SET FDO_LIB_PATH_3_4=D:/fdo-sdk/x86/3.4/Lib
+SET FDO_LIB_PATH_3_5=D:/fdo-sdk/x86/3.5/Lib
+SET FDO_LIB_PATH_3_6=D:/fdo-sdk/x86/3.6/Lib
+SET FDO_LIB_PATH=%FDO_LIB_PATH_3_6%
+SET FDO_LIB_PATH64_3_5=D:/fdo-sdk/x64/3.5/Lib
+SET FDO_LIB_PATH64_3_6=D:/fdo-sdk/x64/3.6/Lib
+SET FDO_LIB_PATH64=%FDO_LIB_PATH64_3_6%
+
+REM SDK Lib Paths (Debug)
+SET FDO_LIB_PATH_DEBUG_3_4=D:/fdo-sdk/x86/3.4/Lib
+SET FDO_LIB_PATH_DEBUG_3_5=D:/fdo-sdk/x86/3.5/Lib
+SET FDO_LIB_PATH_DEBUG_3_6=D:/fdo-sdk/x86/3.6/Lib
+SET FDO_LIB_PATH_DEBUG=%FDO_LIB_PATH_DEBUG_3_6%
+SET FDO_LIB_PATH64_DEBUG_3_5=D:/fdo-sdk/x64/3.5/Lib
+SET FDO_LIB_PATH64_DEBUG_3_6=D:/fdo-sdk/x64/3.6/Lib
+SET FDO_LIB_PATH64_DEBUG=%FDO_LIB_PATH64_DEBUG_3_6%
+
+SET MG_PATH_OS_2_1=D:/mg-2.1/MgDev
+SET MG_PATH_OS_2_2=D:/mg-2.2/MgDev
+SET MG_PATH_ENT_2010=D:/mg-2.1/MgDev
+SET MG_PATH_ENT_2011=D:/mg-2.2/MgDev
 SET MG_PATH_ENT_2012=D:/Development/MapGuideSVN/trunk/MgDev
 REM SET MG_PATH=$(MG_PATH_OS_2_1)
 SET MG_PATH=D:/mg-trunk/MgDev
